f99ef0a8f340fe254c68a68581f6b01c4b3a0084
Gael Cascioli <gaelc@umbc.edu>
2023-09-27 13:17:11 -0400

    Finished installation script

--- simple_install.sh
+++ simple_install.sh
@@ -16,52 +16,53 @@ BOOST_VERSION="1.82.0"
 ######################################################################################################
 # Create and activate environment
 echo "...... Creating conda env ......."
-###conda create --name $ENV_NAME python=3.8 --channel default --channel anaconda 
+conda create --name $ENV_NAME python=3.8 --channel default --channel anaconda 
 eval "$(conda shell.bash hook)"
 conda activate $ENV_NAME
 
 # Install dependencies
 echo "...... Installing dependencies  ......."
-#pip install $MAIN_FLD -r requirements.txt
+pip install $MAIN_FLD -r requirements.txt
 
 # Move into the lib directory
 mkdir $LIB_FLD
 cd $LIB_FLD
 
 # Install Embree 2
-###echo "...... Installing Embree 2 ......."
-###DWN_URL="https://github.com/embree/embree/releases/download/v${EMBREE2_VERSION}/"
-###wget "${DWN_URL}embree-${EMBREE2_VERSION}.x86_64.linux.tar.gz"
-###tar -xf "embree-${EMBREE2_VERSION}.x86_64.linux.tar.gz"
-###
-###cd "embree-$EMBREE2_VERSION".x86_64.linux
-###source embree-vars.sh
-###cd ..
-###
-#### Install Embree 3
-###echo "...... Installing Embree 2 ......."
-###DWN_URL="https://github.com/embree/embree/releases/download/v${EMBREE3_VERSION}/"
-###wget "${DWN_URL}embree-${EMBREE3_VERSION}.x86_64.linux.tar.gz"
-###tar -xf "embree-${EMBREE3_VERSION}.x86_64.linux.tar.gz"
-###cd "embree-$EMBREE3_VERSION".x86_64.linux
-###source embree-vars.sh
-###cd ..
-###
-#### Clone python embree 
-###git clone https://github.com/sampotter/python-embree
-###cd python-embree
-#### NOTE: temporary fix
-###LINE_TO_COMMENT="rtcSetDeviceErrorFunction(self._device, simple_error_function, NULL);"
-###sed -i "/$LINE_TO_COMMENT/s/^/# /" embree.pyx
-###
-#### Install python embree
-###pip install .
-###cd ..
+echo "...... Installing Embree 2 ......."
+DWN_URL="https://github.com/embree/embree/releases/download/v${EMBREE2_VERSION}/"
+wget "${DWN_URL}embree-${EMBREE2_VERSION}.x86_64.linux.tar.gz"
+tar -xf "embree-${EMBREE2_VERSION}.x86_64.linux.tar.gz"
+
+cd "embree-$EMBREE2_VERSION".x86_64.linux
+source embree-vars.sh
+cd ..
+
+# Install Embree 3
+echo "...... Installing Embree 2 ......."
+DWN_URL="https://github.com/embree/embree/releases/download/v${EMBREE3_VERSION}/"
+wget "${DWN_URL}embree-${EMBREE3_VERSION}.x86_64.linux.tar.gz"
+tar -xf "embree-${EMBREE3_VERSION}.x86_64.linux.tar.gz"
+cd "embree-$EMBREE3_VERSION".x86_64.linux
+source embree-vars.sh
+cd ..
+
+# Clone python embree 
+git clone https://github.com/sampotter/python-embree
+cd python-embree
+# NOTE: temporary fix
+LINE_TO_COMMENT="rtcSetDeviceErrorFunction(self._device, simple_error_function, NULL);"
+sed -i "/$LINE_TO_COMMENT/s/^/# /" embree.pyx
+
+# Install python embree
+pip install .
+cd ..
 
 # Install CGAL & Boost
-###DWN_URL="https://github.com/CGAL/cgal/releases/download/v${CGAL_VERSION}/CGAL-${CGAL_VERSION}.tar.xz"
-###wget "${DWN_URL}"
-###tar -xf "CGAL-${CGAL_VERSION}.tar.xz"
+echo "...... Installing CGAL ......."
+DWN_URL="https://github.com/CGAL/cgal/releases/download/v${CGAL_VERSION}/CGAL-${CGAL_VERSION}.tar.xz"
+wget "${DWN_URL}"
+tar -xf "CGAL-${CGAL_VERSION}.tar.xz"
 
 VRS_UND="${BOOST_VERSION//./_}"
 DWN_URL="https://boostorg.jfrog.io/artifactory/main/release/${BOOST_VERSION}/source/boost_${VRS_UND}.tar.gz"
@@ -73,7 +74,7 @@ tar -xf "boost_${VRS_UND}.tar.gz"
 git clone https://github.com/steo85it/py-cgal-aabb.git
 cd py-cgal-aabb
 
-BOOST_INCLUDE="../boost_${VRS_UND}/include"
+BOOST_INCLUDE="../boost_${VRS_UND}"
 CGAL_INCLUDE="../CGAL-${CGAL_VERSION}/include"
 BOOST_ABS="$(realpath $BOOST_INCLUDE)"
 CGAL_ABS="$(realpath $CGAL_INCLUDE)"
@@ -82,6 +83,5 @@ sed  -i "13a\"$BOOST_ABS\",\"$CGAL_ABS\"" setup.py
 python setup.py build_ext --inplace
 pip install .
 
-
-
-## rm -f *.tar*
+cd ..
+rm -f *.tar*
