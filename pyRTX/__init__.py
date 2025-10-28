3ed0f411e1ead72c250e7f2b8909a0d9d590de42
Gael Cascioli <gaelc@umbc.edu>
2025-10-28 11:52:05 -0400

    embree vars

--- pyRTX/__init__.py
+++ pyRTX/__init__.py
@@ -1,47 +1,23 @@
 """
 pyRTX - Non-gravitational forces modelling using raytracing
 """
-import sys
 
 # Import version
 __version__ = "0.1.0"
 
-# Automatically configure Embree environment (Linux only)
-if sys.platform == 'linux':
-    from pyRTX.embree_env import _embree_configured
-else:
-    _embree_configured = False
-
-# Now try to import embree
+# Try to import embree
 try:
     import embree
     EMBREE_AVAILABLE = True
 except ImportError as e:
     EMBREE_AVAILABLE = False
     import warnings
-    
-    if sys.platform == 'linux':
-        if _embree_configured:
-            warnings.warn(
-                f"Embree environment was configured but import failed: {e}\n"
-                "You may need to reinstall pyRTX or check your Embree installation.\n"
-                "Run: python install_deps.py",
-                ImportWarning
-            )
-        else:
-            warnings.warn(
-                "Embree is not available. Embree environment could not be configured.\n"
-                "Some features may be disabled.\n"
-                "Run: python install_deps.py",
-                ImportWarning
-            )
-    else:
-        warnings.warn(
-            f"Embree auto-configuration is only supported on Linux.\n"
-            f"Current platform: {sys.platform}\n"
-            "Please manually configure Embree environment or run install_deps.py",
-            ImportWarning
-        )
+    warnings.warn(
+        f"Embree is not available: {e}\n"
+        "Some features may be disabled.\n"
+        "To install Embree support, run: python install_deps.py",
+        ImportWarning
+    )
 
 # Export key components
 from pyRTX.defaults import dFloat, dInt
